3f889c8e1c9b2f08887cbdffd198c0c0c16991aa
Daniela Reijs <danielareijs@gmail.com>
2021-06-01 22:12:38 +0200

    Added price column to event. Basic structure of event show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_135145) do
+ActiveRecord::Schema.define(version: 2021_06_01_191021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_135145) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active", default: true
+    t.integer "price"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 User.create( first_name: "Maria", last_name: "Sharapova", email: "maria@maria.com", username: "meatballz", password: "qwerty", phone_number: "0123456789", about: "Since i retired from competitive tennis, I now cook Swedish meatballs", allergies: ["peanuts", "clay courts"] )
 User.create( first_name: "Björn", last_name: "Borg", email: "bjorn@bjorn.com", username: "bjorne", password: "qwerty", phone_number: "0123456789", about: "Since i retired from competitive tennis, I now make underwear and enjoy meatballs", allergies: ["John McEnroe"] )
-Event.create( title: "Maria's Meatballz!", description: "tennis ball sized authentic Swedish meatballs, just like Björn used to make!", address: "Ålgrytevägen 5, 127 32 Skärholmen, Sweden", ingredients: "mince meat, onions, salt, butter, pepper", available_seats: 8, user: User.first)
-Event.create( title: "Maria's Sill & Potatis!", description: "Traditional Swedish Sill & Potatoes! with a russian serving of tennis", address: "LM Ericssons Väg 14, 126 27 Hägersten, Sweden", ingredients: "Sill (herring), potatoes", available_seats: 4, user: User.first)
+Event.create( title: "Maria's Meatballz!", description: "tennis ball sized authentic Swedish meatballs, just like Björn used to make!", address: "Ålgrytevägen 5, 127 32 Skärholmen, Sweden", ingredients: "mince meat, onions, salt, butter, pepper", available_seats: 8, price: 100, user: User.first)
+Event.create( title: "Maria's Sill & Potatis!", description: "Traditional Swedish Sill & Potatoes! with a russian serving of tennis", address: "LM Ericssons Väg 14, 126 27 Hägersten, Sweden", ingredients: "Sill (herring), potatoes", available_seats: 4, price: 85, user: User.first)
 Reservation.create( user: User.last, event: Event.first, status: "pending", message: "Hi, I hate John" )
 Reservation.create( user: User.last, event: Event.last, status: "pending", message: "Hi, I hate McEnroe" )
 Chatroom.create( event: Event.first )
