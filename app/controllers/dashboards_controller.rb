672a28ae8065c484226b4d69864addd47c7f4a3b
Rui Ribeiro <ribeiro.r.rui@gmail.com>
2021-06-02 16:45:00 +0200

    Added Dashboard actions and some styling

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,28 @@
+class DashboardsController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @reservations = Reservation.where(user_id: @user)
+    @events = Event.where(user_id: @user)
+    authorize @user
+    @reservations = @user.reservations
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    authorize @user
+    redirect_to dashboard_path(@user)
+    @user.save
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :address, :phone_number, :about, :photo, :allergies)
+  end
+end
