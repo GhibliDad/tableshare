01b13cbd3fe0d9e78ec8d14741995f381c9df90c
Towa Shimizu <towashimizu@gmail.com>
2021-06-02 12:51:50 +0200

    cloudinary set up

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,7 @@ class Event < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_one :chatroom, dependent: :destroy
 
+  has_one_attached :photo
+
   validates :title, :description, :address, :ingredients, :available_seats, presence: true
 end
