c4bf045b3959fe5865b398eeb8234be8b2c4a695
Rui Ribeiro <ribeiro.r.rui@gmail.com>
2021-06-03 17:45:44 +0200

    added date to migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_121459) do
+ActiveRecord::Schema.define(version: 2021_06_03_153820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2021_06_02_121459) do
     t.integer "price"
     t.float "latitude"
     t.float "longitude"
+    t.date "date"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
