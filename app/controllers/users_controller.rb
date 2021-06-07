db03adb9113c5da0559e1d9fabdd2cafe966ef29
Rui Ribeiro <ribeiro.r.rui@gmail.com>
2021-06-07 10:45:13 +0200

    small_fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class DashboardsController < ApplicationController
+class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reservations = Reservation.where(user_id: @user)
