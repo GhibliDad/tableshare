b2c96525a5526897d83fba564654526e1c65c62a
Towa Shimizu <towashimizu@gmail.com>
2021-06-07 18:42:48 +0200

    some changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :events, except: [:destroy] do
-    resources :reservations, only: [:create] do
-      resources :messages, only: :create
+    resources :messages, only: :create
+      resources :reservations, only: :create do
       resources :reviews, only: [:new, :create]
     end
   #   resources :chatrooms, only: [:create]
