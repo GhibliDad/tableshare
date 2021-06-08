6f9bf4181858d1b532458aa86badb543d4298fe5
Towa Shimizu <towashimizu@gmail.com>
2021-06-08 09:55:24 +0200

    did some dashboard setup, unfinished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
     @events = policy_scope(Event).order(created_at: :desc)
+  end
+
+  def dashboard
   end
 end
