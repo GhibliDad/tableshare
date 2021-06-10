4e9c71e2e14d56e6227bb7207ee826238fd4f195
Towa Shimizu <towashimizu@gmail.com>
2021-06-10 15:47:03 +0200

    modal half fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,10 +28,7 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
+
 
 var scrolled = false;
 
@@ -66,7 +63,8 @@ document.addEventListener('turbolinks:load', () => {
   initFlatpickr();
   initAutocomplete();
   mapHighlights();
-  reverseHighlights()
+  reverseHighlights();
+  navTabs();
   // setInterval(updateScroll,1000);
 });
 
